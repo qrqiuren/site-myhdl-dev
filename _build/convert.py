5fa95e9fab5803c74d114ac89e2951db70fde060
Jan Decaluwe <jan@jandecaluwe.com>
2013-12-24 16:33:17 +0100

    mep 103 and 105

--- _build/convert.py
+++ _build/convert.py
@@ -6,8 +6,25 @@ fn = sys.argv[1]
 heading = re.compile(r'([^=]*).*')
 code_inline = re.compile(r"''(.*?)''")
 escape = re.compile(r"%%(.*?)%%")
+emphasis = re.compile(r"//(.*?)//")
+code = re.compile(r'<code>')
 code_python = re.compile(r'<code python>')
+code_myhdl = re.compile(r'<code myhdl>')
+code_verilog = re.compile(r'<code verilog>')
+code_vhdl = re.compile(r'<code vhdl>')
 endcode = re.compile(r'</code>')
+wikilink = re.compile(r'\[\[([^|]*)\]\]')
+link = re.compile(r'\[\[(.*?)\|(.*?)\]\]')
+
+def wikilinkrepl(match):
+    id = match.group(1)
+    id = id.replace(r':', r'/')
+    return '[' + id + ']'
+
+def linkrepl(match):
+    url = match.group(1)
+    text = match.group(2) 
+    return '[' + text + ']' + '(' + url + ')' 
 
 with open(fn) as f:
     print '---'
@@ -31,7 +48,14 @@ with open(fn) as f:
         else:
             line = line[:-1]
             line = escape.sub(r'\1', line)
+            line = emphasis.sub(r'*\1*', line)
             line = code_inline.sub(r'`\1`', line)
+            line = code.sub(r'```', line)
             line = code_python.sub(r'```python', line)
+            line = code_myhdl.sub(r'```python', line)
+            line = code_verilog.sub(r'```verilog', line)
+            line = code_vhdl.sub(r'```vhdl', line)
             line = endcode.sub(r'```', line)
+            line = wikilink.sub(wikilinkrepl, line)
+            line = link.sub(linkrepl, line)
             print line
