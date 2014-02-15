0de9f77a23267eeb8b304b0d66a12ea6d7ae807c
Jan Decaluwe <jan@jandecaluwe.com>
2014-02-15 18:15:35 +0100

    Urubu version check

--- _python/validators.py
+++ _python/validators.py
@@ -1,4 +1,16 @@
 import re
+from distutils.version import StrictVersion
+import urubu
+
+urubu_version_required = "0.2.1"
+
+urubu_version_error = """\
+Urubu version should be >= {}
+Upgrade with: "pip install --upgrade urubu"
+""".format(urubu_version_required)
+
+if StrictVersion(urubu.__version__) < StrictVersion(urubu_version_required):
+    raise AssertionError(urubu_version_error) 
 
 hglib_import_error = """\
 The layout validators for this site require hglib.
