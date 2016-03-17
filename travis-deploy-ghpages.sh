3714011ce48437fea9d569c662627a9bc99563b4
Keerthan Jaic <jckeerthan@gmail.com>
2016-03-17 10:32:07 -0400

    travis: test git-log

--- travis-deploy-ghpages.sh
+++ travis-deploy-ghpages.sh
@@ -4,8 +4,9 @@ rev=$(git rev-parse --short HEAD)
 repo_url="https://myhdl-bot:$GH_TOKEN@github.com/myhdl/site-myhdl-dev.git"
 pages_dir=~/myhdl/ghpages
 
+git log meps/msp-105.md
 git clone -b gh-pages $repo_url $pages_dir
-rsync -avz --delete --exclude .git --exclude CNAME _build/ $pages_dir/
+rsync -az --delete --exclude .git --exclude CNAME _build/ $pages_dir/
 
 cd $pages_dir
 if [[ -n $(git status -s) ]]; then
