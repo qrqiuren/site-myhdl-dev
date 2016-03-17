79962a0ec84718adac015287dde50b410623aabd
Keerthan Jaic <jckeerthan@gmail.com>
2016-03-17 11:21:53 -0400

    improve deploy script

--- travis-deploy-ghpages.sh
+++ travis-deploy-ghpages.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-rev=$(git rev-parse --short HEAD)
+#NOTE: GH_TOKEN is a secret variable. DO NOT echo it anywhere.
+msg=$(git log -1 --format="rebuild pages at %h%nAuthor: %an%n%B")
+echo $msg
 repo_url="https://myhdl-bot:$GH_TOKEN@github.com/myhdl/site-myhdl-dev.git"
 pages_dir=~/myhdl/ghpages
 
@@ -8,10 +10,13 @@ git clone -b gh-pages $repo_url $pages_dir
 rsync -az --delete --exclude .git --exclude CNAME _build/ $pages_dir/
 
 cd $pages_dir
-if [[ -n $(git status -s) ]]; then
-  git config user.name "MyHDL Bot"
-  git config user.email "myhdl-bot@users.noreply.github.com"
-  git config push.default simple
-  git commit --all -m "rebuild pages at ${rev}"
-  git push -q
+if [[ -z $(git status -s) ]]; then
+  echo "No changes. Exiting."
+  exit
 fi
+
+git config user.name "MyHDL Bot"
+git config user.email "myhdl-bot@users.noreply.github.com"
+git config push.default simple
+git commit --all -m "rebuild pages at ${msg}"
+git push -q
